020d58e224bb689b159ec795b8b46231505db6c4
BoXueee <boxue@cn.ibm.com>
2016-06-08 00:32:56 +0800

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -10,13 +10,12 @@ r1 = requests.put("https://boxue.cloudant.com/boxue_testdb",
 
 if r1.status_code == 201:
    print 'Create database boxue_testdb succeeded.'
-elif r1.status_code == 412:
-   print 'Database boxue_testdb already exist.'
 elif r1.status_code == 401:
    print 'Incorrect credential.'
    quit()         		 
 else:
    print 'Error occurred while creating database, rc =', r1.status_code
+   print r1.json()
 			 
 r2 = requests.put("https://boxue.cloudant.com/boxue_testdb/boxue_testdoc",
              auth=(username, password),
@@ -25,10 +24,9 @@ r2 = requests.put("https://boxue.cloudant.com/boxue_testdb/boxue_testdoc",
 			 
 if r2.status_code == 201:
    print 'Create document boxue_testdoc succeeded.'
-elif r2.status_code == 409:
-   print 'Document boxue_testdoc already exist.'
 else:   
-   print 'Error occurred while creating document, rc =', r2.status_code		 
+   print 'Error occurred while creating document, rc =', r2.status_code		
+   print r2.json()   
 
 r3 = requests.get("https://boxue.cloudant.com/boxue_testdb/boxue_testdoc",
              auth=(username, password))		
@@ -40,4 +38,5 @@ if r3.status_code == 200:
    print 'text: ', r3.text 
    print 'json: ', r3.json()
 else:
-   print 'Error occurred while retrieving document boxue_testdoc, rc =', r3.status_code		   
+   print 'Error occurred while retrieving document boxue_testdoc, rc =', r3.status_code		
+   print r3.json()   
